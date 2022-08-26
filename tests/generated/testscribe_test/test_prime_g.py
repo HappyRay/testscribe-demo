2014e51874d7d7027922a122d9e52a7951fe9220
Ray Yang <ruiguo@gmail.com>
2022-08-26 14:44:30 -0700

    Add an is_prime example

---
+++ tests/generated/testscribe_test/test_prime_g.py
@@ -0,0 +1,21 @@
+from testscribe_test.prime import is_prime
+
+
+def test_is_prime_not_prime_1():
+    result = is_prime(n=15)
+    assert result is False
+
+
+def test_is_prime_2_is_prime():
+    result = is_prime(n=2)
+    assert result is True
+
+
+def test_is_prime_not_prime():
+    result = is_prime(n=4)
+    assert result is False
+
+
+def test_is_prime_is_prime():
+    result = is_prime(n=3)
+    assert result is True
