ad85cbee140c25178606843b284cb8e11c5cc749
Ray Yang <ruiguo@gmail.com>
2022-09-20 16:49:34 -0700

    Add a demo of throwing an exception in a mock call

--- tests/generated/tsdemo/test_simple_mock_g.py
+++ tests/generated/tsdemo/test_simple_mock_g.py
@@ -1,7 +1,20 @@
 import tsdemo.service
 from testscribe.api.mock_api import get_normalized_mock_calls
 from unittest.mock import ANY, call, create_autospec
+import pytest
 from tsdemo.simple_mock import search_name
+
+
+def test_search_name_exception_is_propagated():
+    m_service: tsdemo.service.Service = create_autospec(spec=tsdemo.service.Service)
+    m_service.search_a_name.side_effect = [Exception('search failed')]
+    with pytest.raises(Exception) as exception_info:
+        search_name(service=m_service, keyword='Bob')
+    assert 'search failed' == str(exception_info.value)
+    m_service_mock_calls = get_normalized_mock_calls(m_service, tsdemo.service.Service)
+    assert m_service_mock_calls == [
+        call.search_a_name(keyword='key: Bob'),
+    ]
 
 
 def test_search_name():
