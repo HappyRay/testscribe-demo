5bbc7c0ae2f3b81551c160f49dcc8f6e59d191d1
Ray Yang <ruiguo@gmail.com>
2022-09-28 14:52:39 -0700

    Add a demo of input aliases

--- tests/setup.py
+++ tests/setup.py
@@ -1,3 +1,4 @@
+from testscribe.api.alias import define_alias
 from testscribe.api.mock_api import patch_with_mock, patch_with_expression
 
 # from tsdemo.patch_function import calculate
@@ -5,5 +6,7 @@ from testscribe.api.mock_api import patch_with_mock, patch_with_expression
 
 def setup():
     print("setup function in tests is called.")
+    define_alias(alias="td", full_str="tsdemo")
+    # define_alias(alias="ps", full_str="tsdemo.person.Person")
     # patch_with_mock(calculate)
     # patch_with_expression(target_str="tsdemo.patch_string.DB_NAME", expression="'test'")
